74f8c3a41baeddab5bd5ce25e095213442f6e318
Mahir <mkojak75@gmail.com>
2024-03-23 17:03:09 +0100

    productList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import ProductList from "./components/ProductCard/ProductList";
+import "./App.scss";
+import {products, categories} from "./helper/data";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header categories={categories} text="ProductList" />
+      <ProductList products={products} />
+        
+      
     </div>
   );
 }
